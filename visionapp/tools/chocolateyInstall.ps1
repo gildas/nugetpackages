74c572a117b38c463c471f49a13b21169be0f789
Gildas Cherruel <gildas.cherruel@inin.com>
2014-02-28 16:25:43 +0900

    New package: VisionApp by ASG Gmbh

---
+++ visionapp/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$packageName = 'visionapp'
+$installerType = 'EXE'
+$url = 'http://cdn.visionapp.com/ASG-RD2012_P8_Plain.zip'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try
+{
+  $tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
+  $exeFile = "ASGRD_Setup.exe"
+  $zipFile = "ASG-RD2012_P8_Plain.zip"
+
+  if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
+  Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
+  Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $exeFile)" -validExitCodes $validExitCodes
+  Write-ChocolateySuccess "$packageName"
+}
+catch
+{
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
