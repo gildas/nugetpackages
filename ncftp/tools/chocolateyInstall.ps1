e30f2dd8248a62412802f17d0e2d5af4023d62d0
Gildas Cherruel <gildas.cherruel@inin.com>
2014-04-02 18:37:34 +0900

    New package: ncftp. Command line FTP client from Unix

---
+++ ncftp/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'ncftp'
+$installerType = 'MSI'
+$url = 'ftp://ftp.ncftp.com/ncftp/binaries/Setup%20NcFTP%203.2.5.msi'
+$silentArgs = '/q' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" ""  -validExitCodes $validExitCodes
