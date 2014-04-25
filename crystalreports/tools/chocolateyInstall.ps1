2d70eb0bf3cc9a19df045c27f41c7ef9c46269b7
Gildas Cherruel <gildas.cherruel@inin.com>
2014-04-25 17:12:50 +0900

    New Project: Crystal Reports

---
+++ crystalreports/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'cr_for_vs'
+$installerType = 'EXE'
+$url = 'http://downloads.businessobjects.com/akdlm/cr4vs2010/CRforVS_13_0_9.exe'
+$url64 = ''
+$silentArgs =
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
