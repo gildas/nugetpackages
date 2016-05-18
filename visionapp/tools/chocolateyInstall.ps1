031b875303590b5db2caa7458d4794343c49e1d7
Gildas Cherruel <gildas.cherruel@inin.com>
2016-05-18 19:05:49 +0900

    Visionapp: 9.0.5222 (2016)

--- visionapp/tools/chocolateyInstall.ps1
+++ visionapp/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'visionapp'
 $installerType = 'EXE'
-$url = 'http://cdn.visionapp.com/ASG-RD2012_P8_Plain.zip'
+$url = 'http://cdn.visionapp.com/ASGRD_latest.zip'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
@@ -8,16 +8,14 @@ try
 {
   $tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
   $exeFile = "ASGRD_Setup.exe"
-  $zipFile = "ASG-RD2012_P8_Plain.zip"
+  $zipFile = "ASGRD_latest.zip"
 
   if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
   Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
   Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
   Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $exeFile)" -validExitCodes $validExitCodes
-  Write-ChocolateySuccess "$packageName"
 }
 catch
 {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
+  throw $_
 }
