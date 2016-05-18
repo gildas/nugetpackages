18d54d69653c79d3a800e8e181a1320791187541
Gildas Cherruel <gildas.cherruel@inin.com>
2016-05-18 18:13:33 +0900

    GoToMeeting: 7.17.0

---
+++ gotomeeting/ChangeLog.md
@@ -0,0 +1,3 @@
+## 2016-05-18 - Version 7.17.0
+- Installs GotoMeeting version 7.17.0, build 4911
+- Fixed the nuspec and the install script to follow the guidelines mentiond during the approval process.
--- gotomeeting/tools/chocolateyInstall.ps1
+++ gotomeeting/tools/chocolateyInstall.ps1
@@ -3,6 +3,7 @@ $installerType = 'MSI'
 # The support page for downloading is: http://support.citrixonline.com/en_US/gotomeeting/downloaddocument/GTMD00053
 # Which redirects the actual download to the following URL
 $url = 'http://support.citrixonline.com/servlet/fileField?retURL=%2Fapex%2FCPDownloadStarter%3FarticleLinkId%3DGTMD00053%26l%3Den_US%26product%3Dgotomeeting&entityId=ka350000000TgcUAAS&field=Content__Body__s'
+http://support.citrixonline.com/en_US/meeting/all_files/G2M010013
 $silentArgs = '/q ALLUSERS=1'
 $validExitCodes = @(0)
 
@@ -16,10 +17,8 @@ try
   Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
   Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
   Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $msiFile)" -validExitCodes $validExitCodes
-  Write-ChocolateySuccess "$packageName"
 }
 catch
 {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
+  throw $_
 }
