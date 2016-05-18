696e765772d99462e8f5e5fe9ca2653be5c722a7
Gildas Cherruel <gildas.cherruel@inin.com>
2016-05-18 18:27:14 +0900

    Added proper zip and msi

--- gotomeeting/tools/chocolateyInstall.ps1
+++ gotomeeting/tools/chocolateyInstall.ps1
@@ -2,17 +2,15 @@
 $installerType = 'MSI'
 # The support page for downloading is: http://support.citrixonline.com/en_US/gotomeeting/downloaddocument/GTMD00053
 # Which redirects the actual download to the following URL
-$url = 'http://support.citrixonline.com/servlet/fileField?retURL=%2Fapex%2FCPDownloadStarter%3FarticleLinkId%3DGTMD00053%26l%3Den_US%26product%3Dgotomeeting&entityId=ka350000000TgcUAAS&field=Content__Body__s'
-http://support.citrixonline.com/en_US/meeting/all_files/G2M010013
+$url = 'http://support.citrixonline.com/servlet/fileField?retURL=%2Fapex%2FCPDownloadStarter%3FarticleLinkId%3DG2MD00196%26l%3Den_US%26product%3DMeeting&entityId=ka338000000GtVFAA0&field=Content__Body__s'
 $silentArgs = '/q ALLUSERS=1'
 $validExitCodes = @(0)
 
 try
 {
   $tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
-  $msiFile = "G2MSetup6.1.1312_IT.msi"
-  $zipFile = "G2M_U.S._v6.1.2_b1312_MSI.zip"
-
+  $msiFile = "G2MSetup7.17.4911_IT.msi"
+  $zipFile = "G2MSetup7.17.4911_IT.zip"
   if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
   Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
   Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
