9a35cb978b812d3a55139a1ecc621f9be376070b
Gildas Cherruel <gildas.cherruel@inin.com>
2016-05-20 10:37:52 +0900

    Turtlemine is inactive

--- turtlemine/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'turtlemine'
-$installerType = 'MSI'
-$url = 'http://turtlemine.googlecode.com/files/TurtleMine_32bit_1.2.0.1.msi'
-$url64 = 'http://turtlemine.googlecode.com/files/TurtleMine_64bit_1.2.0.1.msi'
-$silentArgs = '/q'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
