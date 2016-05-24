8e07b53507a93a9b1c6b5fde348ddae96d0ff022
Gildas Cherruel <gildas.cherruel@inin.com>
2016-05-24 10:56:41 +0900

    Removed the try catch as it is implemented at a higher level

--- visionapp/tools/chocolateyInstall.ps1
+++ visionapp/tools/chocolateyInstall.ps1
@@ -4,18 +4,11 @@ $url = 'http://cdn.visionapp.com/ASGRD_latest.zip'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-try
-{
-  $tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
-  $exeFile = "ASGRD_Setup.exe"
-  $zipFile = "ASGRD_latest.zip"
+$tempDir = Join-Path $(Join-Path $env:TEMP "chocolatey") "$packageName"
+$exeFile = "ASGRD_Setup.exe"
+$zipFile = "ASGRD_latest.zip"
 
-  if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
-  Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
-  Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $exeFile)" -validExitCodes $validExitCodes
-}
-catch
-{
-  throw $_
-}
+if (! [System.IO.Directory]::Exists($tempDir)) { [System.IO.Directory]::CreateDirectory($tempDir) }
+Get-ChocolateyWebFile "$packageName" "$(Join-Path $tempDir $zipFile)" $url
+Get-ChocolateyUnzip "$(Join-Path $tempDir $zipFile)" $tempDir "" "$packageName"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$(Join-Path $tempDir $exeFile)" -validExitCodes $validExitCodes
